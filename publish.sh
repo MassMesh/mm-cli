742829337aed5758f0190a5e1728a74f8ebf8c7f
JamesVorder <james@bip44.email>
2020-05-05 21:05:21 -0400

    Bump version; fix publish script.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # For further instructions on setting up a Pypi account, etc. see the following:
 # https://packaging.python.org/tutorials/packaging-projects/
+rm -rf dist
 python setup.py sdist bdist_wheel
 twine upload dist/*
